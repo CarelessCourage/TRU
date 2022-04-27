69b1ff0a9342dd54856878bb6709ab03f547ab00
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2022-04-27 14:48:43 +0100

    Make media responsive

--- src/components/imglist.vue
+++ src/components/imglist.vue
@@ -45,12 +45,13 @@ import mediaArray from "../store/media.js"
   text-transform: uppercase;
   margin-bottom: 5em;
 
-  @media (max-width: 1350px) {
-    grid-template-columns: repeat(4, 1fr);
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (max-width: 1150px) {
-    grid-column: span 5;
+  @media (max-width: 700px) {
+    //grid-column: span 5;
+    grid-template-columns: repeat(1, 1fr);
   }
 }
 
